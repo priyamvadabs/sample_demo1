9ec8797571186f964b5b7ad1b30e30453ad45833
priyamvadabs <priyamvada@kompetenzen.in>
2025-07-26 10:06:52 +0530

    changes to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Home = () => {
+function Home() {
   return (
-    <div>Home</div>
-  )
+    <div style={{ padding: '20px' }}>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main landing page.</p>
+      <Link to="/about">
+        <button>Go to About Page</button>
+      </Link>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
